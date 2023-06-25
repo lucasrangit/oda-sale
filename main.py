e461ce102747f87b187b4e219bbfe53f6ae478e6
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2023-06-25 17:51:44 +0200

    TODO

--- main.py
+++ main.py
@@ -126,6 +126,7 @@ def main():
     print(f"Found {len(product_urls)} product URLs")
 
     # fetch product pages and build product list
+    # try using concurrent.futures https://stackoverflow.com/a/74080466
     print("Fetching products", end = '', flush = True)
     asyncio.run(parse_product_tables_parallel(product_urls))
     print(f"\nFound {len(product_list)} product details")
